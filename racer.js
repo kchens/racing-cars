06e5b753d1f76f3f0f5341df5b756cc01d2777cd
Kevin K Chen <kevin.kang.chen@gmail.com>
2015-02-10 15:52:24 -0800

    first commit

--- racer.js
+++ racer.js
@@ -1,49 +1,14 @@
-function update_player_position(player) {
-  player_obj = document.querySelector(player);
-  td_active = player_obj.querySelector(".active");
-  td_active_next = td_active.nextElementSibling
+function updatePlayerPosition(playerIndex, position) {
+  playerCells = document.querySelectorAll("#player" + (playerIndex) + "_strip > td");
 
-  if ( td_active_next !== null ) {
-    td_active.nextElementSibling.className = "active";
-    td_active.className = "";
-  } else {
-    winner(player);
-    playAgain();
-  }
+  Array.prototype.forEach.call( playerCells,(function(cell, index) {
+    if (index == position) {
+      cell.className = "active";
+    }
+    else {
+      cell.className = "";
+    }
+  }));
 }
 
-
-function on_key_press(e) {
-  if (e.keyCode === 90) {
-    update_player_position('#player1_strip');
-  } else if (e.keyCode === 77) {
-    update_player_position('#player2_strip');
-  }
-}
-
-function winner(player) {
-  alert(player + " Wins!");
-}
-
-function playAgain() {
-  var input = prompt("Play again? Y/N");
-  switch(input){
-    case 'Y':
-      newGame();
-      document.addEventListener('keydown', on_key_press, false);
-    case 'N':
-  }
-}
-
-var newGame = function() {
-  var track = document.querySelectorAll("tr td");
-  for (var i = 0; i < track.length ; i++) {
-    track[i].className = "";
-  }
-  track[0].className = 'active';
-  track[track.length / 2].className = "active";
-}
-
-window.onload = function() {
-  document.addEventListener('keydown', on_key_press, false);
-}
+updatePlayerPosition(1)
