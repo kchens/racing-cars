ea8b49a625803165bb8fbe745b5149f42cb23387
Kevin K Chen <kevin.kang.chen@gmail.com>
2015-02-10 16:41:02 -0800

    added getPlayerCells, getPlayerPosition

--- racer.js
+++ racer.js
@@ -1,6 +1,20 @@
-function updatePlayerPosition(playerIndex, position) {
+function getPlayerCells(playerNum) {
   // > get the descendent html tags from the query selector
-  playerCells = document.querySelectorAll("#player" + (playerIndex) + "_strip > td");
+  return document.querySelectorAll("#player" + (playerNum) + "_strip > td");
+}
+
+function getPlayerPosition(playerNum) {
+  var playerCells = getPlayerCells(playerNum);
+  Array.prototype.forEach.call( playerCells, function(cell, index) {
+    if (cell.classList.contains("active") == true) {
+      console.log("hello");
+      console.log(index);
+    }
+  });
+}
+
+function updatePlayerPosition(playerNum, position) {
+  var playerCells = getPlayerCells(playerNum);
 
   Array.prototype.forEach.call( playerCells,(function(cell, index) {
     if (index == position) {
@@ -14,9 +28,11 @@ function updatePlayerPosition(playerIndex, position) {
 
 function movePlayer(event) {
   if (event.keyCode == 90 ) { // 'z'
+    getPlayerPosition(1);
     updatePlayerPosition(1, 5);
   }
   else { (event.keyCode == 77)  // 'm'
+    getPlayerPosition(2);
     updatePlayerPosition(2, 5);
   }
 }
@@ -24,3 +40,5 @@ function movePlayer(event) {
 window.onload = function() {
   document.addEventListener('keyup', movePlayer, false);
 }
+
+getPlayerPosition(1)
