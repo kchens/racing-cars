f6fe12919818149a127a6e079e071e15bd9d5a61
Kevin K Chen <kevin.kang.chen@gmail.com>
2015-02-10 16:20:16 -0800

    bound key codes to updatePlayerPosition

--- racer.js
+++ racer.js
@@ -1,4 +1,5 @@
 function updatePlayerPosition(playerIndex, position) {
+  // > get the descendent html tags from the query selector
   playerCells = document.querySelectorAll("#player" + (playerIndex) + "_strip > td");
 
   Array.prototype.forEach.call( playerCells,(function(cell, index) {
@@ -11,4 +12,15 @@ function updatePlayerPosition(playerIndex, position) {
   }));
 }
 
-updatePlayerPosition(1)
+function movePlayer(event) {
+  if (event.keyCode == 90 ) { // 'z'
+    updatePlayerPosition(1, 5);
+  }
+  else { (event.keyCode == 77)  // 'm'
+    updatePlayerPosition(2, 5);
+  }
+}
+
+window.onload = function() {
+  document.addEventListener('keyup', movePlayer, false);
+}
